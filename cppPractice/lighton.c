d5dc01fbdfaaa1b7f80c2f7a69849cb4287b12b0
50infivedays <wlinpices@gmail.com>
2013-10-26 22:36:18 +0800

    update

---
+++ cppPractice/lighton.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXN 1000+10
+int a[MAXN];
+int main()
+{
+    int i, j, n, k, first=1;
+    memset(a, 0, sizeof(a));
+
+}
---
+++ cppPractice/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    i = 3.13 +3;
+    cout << i << endl;
+    return 0;
+}
